6d7f40f7c634ea1e993540d0752b8d4b6c06d84c
nrichardson14 <nicholerichardson4316@gmail.com>
2020-06-07 17:19:19 +0900

    footer fix

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let counter = 1; 
     const size = carouselImages[0].clientWidth; 
 
+
     carouselSlide.style.transform = 'translateX(' + (-size*counter) + 'px)';
 
     //Button listeners
@@ -28,21 +29,18 @@ document.addEventListener('DOMContentLoaded', function(){
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     });
 
-    carouselSlide.addEventListener('transitionend', () => {
-        if (carouselImages[counter].id === 'lastClone'){
+    carouselSlide.addEventListener('transitionend',()=>{
+      if (carouselImages[counter].id === 'lastClone'){
+         carouselSlide.style.transition = "none";  
+         counter = carouselImages.length -2 ;
+         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      }
+      if (carouselImages[counter].id === 'firstClone'){
           carouselSlide.style.transition = "none";  
-          counter = carouselImages.length -2 ;
+          counter = carouselImages.length - counter;
           carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-        }
-        if (carouselImages[counter].id === 'firstClone'){
-            carouselSlide.style.transition = "none";  
-            counter = carouselImages.length - counter;
-            carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
           }
     });
-
-
-
-    
+   
 
 })
